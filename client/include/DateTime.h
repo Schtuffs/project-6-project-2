8c10d7f065a839ed6c3622da6571decf8997f7ed
Harris Ibrahimi <98238342+SUP3RBOB@users.noreply.github.com>
2026-04-07 18:43:43 -0400

    Working on client
    - Created date time class
    - Implemented the stuff the client is gonna do (untested)

---
+++ client/include/DateTime.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+#include "sockets/Packet.h"
+#include <string>
+
+class DateTime {
+private:
+    int32_t month;
+    int32_t day;
+    int32_t year;
+    int32_t hour;
+    int32_t minute;
+    int32_t second;
+
+public:
+    DateTime(int32_t month, int32_t day, int32_t year, int32_t hour, int32_t minute, int32_t second);
+    DateTime(std::string dateTime);
+
+    int32_t GetMonth() const;
+    int32_t GetDay() const;
+    int32_t GetYear() const;
+    int32_t GetHour() const;
+    int32_t GetMinute() const;
+    int32_t GetSecond() const;
+
+    friend Packet& operator <<(Packet& packet, DateTime& dateTime);
+    friend Packet& operator >>(Packet& packet, DateTime& dateTime);
+};
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,6 +1,54 @@
+#include "DateTime.h"
+#include "sockets/ClientSocket.h"
+#include "sockets/Packet.h"
 #include "sockets/Sockets.h"
+#include <boost/algorithm/string/classification.hpp>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include <vector>
+
+#define TELEMETRY_FILE "../resources/katl-kefd-B737-700.txt"
 
 int main(void) {
+    int32_t clientID = -1;
+
+    ClientSocket clientSocket = ClientSocket(CONNECTION_TYPE::UDP);
+
+    Packet startupPacket = clientSocket.receive(0, sizeof(int32_t));
+    if (!startupPacket.isValid()) {
+        std::cout << "Failed to receive startup packet containing client ID." << std::endl;
+        //return EXIT_FAILURE;
+    }
+
+    startupPacket >> clientID;
+
+    std::ifstream telemetryFile = std::ifstream(TELEMETRY_FILE);
+    if (!telemetryFile.is_open()) {
+        std::cout << "Failed to open file: " << TELEMETRY_FILE << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string line = "";
+    while (std::getline(telemetryFile, line)) {
+        std::vector<std::string> telemetry = std::vector<std::string>();
+        boost::trim_if(line, boost::is_any_of(" "));
+        boost::split(telemetry, line, boost::is_any_of(","));
+        
+        float remainingFuel = std::stof(telemetry.back());
+        telemetry.pop_back();
+        
+        DateTime dateTime = DateTime(telemetry.back());
+        
+        Packet packet = Packet(sizeof(int32_t) + sizeof(DateTime));
+        packet << dateTime << remainingFuel;
+
+        clientSocket.send(packet);
+    }
+
     return 0;
 }
 
